a2b6f764e843777a3906b616c13fdf5e64baee2f
陆子虚 <lurongv@qq.com>
2025-04-22 17:16:27 +0800

    ✨feature: 支持更多 hash 算法

--- index.ts
+++ index.ts
@@ -1,19 +1,45 @@
+import { CryptoHasher } from "bun"
 import { FastMCP } from "fastmcp"
-import md5 from "md5"
 import { z } from "zod"
 
 const server = new FastMCP({
-    name: "md5",
+    name: "hash",
     version: "1.0.0",
 })
 
 server.addTool({
-    name: "md5",
-    description: "计算字符串的MD5值",
+    name: "hash",
+    description: "计算字符串的 hash 值",
     parameters: z.object({
-        input: z.string().describe("需要计算MD5值的字符串"),
+        input: z.string().describe("需要计算 hash 值的字符串"),
+        algorithm: z
+            .enum([
+                "blake2b256",
+                "blake2b512",
+                "md4",
+                "md5",
+                "ripemd160",
+                "sha1",
+                "sha224",
+                "sha256",
+                "sha384",
+                "sha512",
+                "sha512-224",
+                "sha512-256",
+                "sha3-224",
+                "sha3-256",
+                "sha3-384",
+                "sha3-512",
+                "shake128",
+                "shake256",
+            ])
+            .describe("hash 算法"),
     }),
-    execute: async ({ input }) => md5(input),
+    execute: async ({ input, algorithm }) => {
+        const hasher = new CryptoHasher(algorithm)
+        hasher.update(input)
+        return hasher.digest("hex")
+    },
 })
 
 server.start({
